8d0129b18a435806ec9fb831666c2a719099e230
mign0n <mign0n.git@gmail.com>
2023-05-17 17:11:52 +0500

    feat: Add the /titles/{title_id}/reviews/{review_id}/ endpoint.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -14,12 +14,22 @@ app_name = 'api'
 
 v1_router = SimpleRouter()
 v1_router.register('categories', CategoryViewSet, basename='category')
-v1_router.register('comments', CommentViewSet, basename='comment')
 v1_router.register('genres', GenreViewSet, basename='genre')
 v1_router.register('genretitle', GenreTitleViewSet, basename='genre-title')
 v1_router.register('reviews', ReviewViewSet, basename='review')
 v1_router.register('titles', TitleViewSet, basename='title')
 
+v1_reviews_router = SimpleRouter()
+v1_reviews_router.register('reviews', ReviewViewSet, basename='review')
+
+v1_comments_router = SimpleRouter()
+v1_comments_router.register('comments', CommentViewSet, basename='comment')
+
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
+    path('v1/titles/<int:title_id>/', include(v1_reviews_router.urls)),
+    path(
+        'v1/titles/<int:title_id>/reviews/<int:review_id>/',
+        include(v1_comments_router.urls),
+    ),
 ]
