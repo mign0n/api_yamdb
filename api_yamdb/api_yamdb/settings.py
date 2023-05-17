b3f8c34d99b2d3c7746690ce3a9baa9086ab3e5d
mign0n <mign0n.git@gmail.com>
2023-05-17 11:22:20 +0500

    feat: Add the /titles/{title_id}/reviews/ endpoint.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -88,6 +88,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
+
 # Internationalization
 
 LANGUAGE_CODE = 'en-us'
