b54cc6c9d486281b9ceff440cdd3faee1b34e26c
mign0n <mign0n.git@gmail.com>
2023-05-18 20:14:28 +0500

    style: Match the code to an accepted standard.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -17,12 +17,12 @@ from api.serializers import (
     GenreSerializer,
     GenreTitleSerializer,
     ReviewSerializer,
+    SignUpSerializer,
     TitleSerializer,
+    TokenSerializer,
 )
 from reviews.models import Category, Comment, Genre, GenreTitle, Title
 from users.models import CustomUser
-
-from api.serializers import SignUpSerializer, TokenSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -58,13 +58,22 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def get_queryset(self) -> QuerySet:
         return self._title.reviews.all()
 
-    def create(self, request: Request, *args, **kwargs) -> Response:
+    def create(
+        self,
+        request: Request,
+        *args: tuple,
+        **kwargs: dict,
+    ) -> Response:
         request.data['author'] = self.request.user.pk
         request.data['title'] = self.kwargs.get('title_id')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=self.get_success_headers(serializer.data),)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=self.get_success_headers(serializer.data),
+        )
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -73,14 +82,15 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 
 class SignUpView(APIView):
-    ''' Отправка письма с кодом подтверждения на email.'''
+    """Отправка письма с кодом подтверждения на email."""
 
     permission_classes = (permissions.AllowAny,)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         try:
             user = CustomUser.objects.get(
-                username=request.data.get('username'))
+                username=request.data.get('username'),
+            )
             code = getattr(user, 'confirmation_code')
             email = getattr(user, 'email')
             if not code and user or code and user:
@@ -117,20 +127,25 @@ class SignUpView(APIView):
 
 
 class TokenView(APIView):
-    '''Получение JWT-токена в обмен на username и confirmation code.'''
+    """Получение JWT-токена в обмен на username и confirmation code."""
 
     permission_classes = (permissions.AllowAny,)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         serializer = TokenSerializer(data=request.data)
         if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         username = serializer.data['username']
         user = get_object_or_404(CustomUser, username=username)
         confirmation_code = serializer.data['confirmation_code']
         if user.confirmation_code != confirmation_code:
-            return Response({'Код подтверждения не верен'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'Код подтверждения не верен'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         else:
             token = AccessToken.for_user(user)
             return Response({'token': str(token)}, status=status.HTTP_200_OK)
